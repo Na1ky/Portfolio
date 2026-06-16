f5a8c4a57fa840cf70256104635c7de10efe8cf0
Na1ky <nicolasdominici.lav@gmail.com>
2026-06-16 17:01:05 +0200

    .

--- Portfolio-Backend/src/services/projectService.ts
+++ Portfolio-Backend/src/services/projectService.ts
@@ -3,7 +3,7 @@ import { getDB } from "../config/database";
 export const getProjects = async () => {
     const db = getDB();
     const collection = db.collection("Projects");
-    return await collection.find().toArray();
+    return await collection.find().sort({ date_achieved: -1 }).toArray();
 };
 
 export const getProjectsCount = async (): Promise<number> => {
